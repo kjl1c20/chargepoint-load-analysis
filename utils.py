4d5e4823c6525df89952145598430a092e608721
kjl1c20 <kangjielee0920@gmail.com>
2026-05-16 00:48:52 +0100

    Add helper function to save harvested data locally in the repo

--- utils.py
+++ utils.py
@@ -1,12 +1,17 @@
 import os
+import pandas as pd
+from datetime import datetime
+import uuid
+import json
 from pathlib import Path
-
 from dotenv import load_dotenv
 from pyiceberg.catalog import load_catalog
 
 load_dotenv(Path(__file__).resolve().parent / ".env")
 
 SENSE_CATALOG_URL = "https://catalog.sdr-sense.org.uk/api/catalog"
+RAW_DATA_DIR = Path("./data/raw")
+METADATA_DIR = Path("./data/metadata")
 
 def connect_to_warehouse(warehouse_slug):
     """Connect to a specific SENSE organisation catalog."""
@@ -28,3 +33,72 @@ def connect_to_warehouse(warehouse_slug):
             "warehouse": warehouse_slug,
         }
     )
+
+
+def save_raw_snapshot(
+    df: pd.DataFrame,
+    dataset_name: str,
+    filters: dict = None,
+    limit = None
+):
+    """
+    Save parquet snapshot + metadata manifest
+    """
+
+    # GENENRATE UNIQUE SNAPSHOT ID
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    short_uuid = str(uuid.uuid4())[:8]
+
+    snapshot_id = f"{timestamp}_{short_uuid}"
+
+    parquet_path = (
+        RAW_DATA_DIR
+        / f"{snapshot_id}.parquet"
+    )
+
+    metadata_path = (
+        METADATA_DIR
+        / f"{snapshot_id}.json"
+    )
+
+    # SAVE PARQUET
+    df.to_parquet(parquet_path, index=False)
+
+    # METADATA MANIFEST
+    metadata = {
+        "snapshot_id": snapshot_id,
+        "dataset_name": dataset_name,
+        "created_at": datetime.now().isoformat(),
+
+        "data": {
+            "rows": int(df.shape[0]),
+            "columns": int(df.shape[1]),
+            "column_names": list(df.columns)
+        },
+
+        "filters": filters or {},
+
+        "limit": limit or "No limit",
+
+        "storage": {
+            "format": "parquet",
+            "path": str(parquet_path)
+        }
+    }
+
+    # -------------------------------------------------
+    # SAVE METADATA
+    # -------------------------------------------------
+    with open(metadata_path, "w") as f:
+        json.dump(metadata, f, indent=4)
+
+    # -------------------------------------------------
+    # LOGGING
+    # -------------------------------------------------
+    print("✅ Snapshot saved")
+    print(f"📦 Snapshot ID: {snapshot_id}")
+    print(f"📂 Data: {parquet_path}")
+    print(f"📝 Metadata: {metadata_path}")
+
+    return snapshot_id
